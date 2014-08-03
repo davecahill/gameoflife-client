188f9ccd739f3c54f0138ec290f64edbefbd92c3
Dave Cahill <dpcahill@gmail.com>
2014-08-03 14:38:30 +0900

    Show message if we fail to contact server

--- javascripts/gol.js
+++ javascripts/gol.js
@@ -1,3 +1,4 @@
+// Simple sleep function for animation
 function sleep(milliseconds) {
   var start = new Date().getTime();
   for (var i = 0; i < 1e7; i++) {
@@ -7,41 +8,70 @@ function sleep(milliseconds) {
   }
 }
 
-var BoardModel = Backbone.Model.extend({
-    urlRoot: 'http://localhost:8080/new/10'
+// Set up info messages
+var InfoMessage = Backbone.Model.extend({
+    messageText: "hello world"
+});
+var InfoMessages = Backbone.Collection.extend({
+  model: InfoMessage
 });
 
+// Set up board model
+var BoardModel = Backbone.Model.extend({
+    urlRoot: 'http://localhost:8080/new/5'
+});
+
+// Set up view
 var AppView = Backbone.View.extend({
   el: '#container',
 
   initialize: function(){
+    this.infoMessages = new InfoMessages();
     this.listenTo(this.model, 'change', this.render);
-    this.model.fetch();
+    this.model.fetch({success: this.serverSuccess, error: this.serverError});
+  },
+
+  serverSuccess: function(model, response) {
+    // Do nothing for now
+  },
+
+  serverError: function(model, response) {
+    appView.logMessage("Contacting Game of Life API server failed (" + response.statusText + "), URL: <i>" + appView.model.urlRoot + "</i>");
+  },
+
+  logMessage: function(msg) {
+    this.infoMessages.add(new InfoMessage({messageText: msg}));
+    this.render();
   },
 
   render: function(){
-    console.log(this.model);
     var states = this.model.attributes.States;
     var html = "<table>";
-    for (var row = 0; row < states.length; row++) {
-      html += "<tr>";
-      for (var cell = 0; cell < states[row].length; cell++) {
-        html += "<td class='";
-        if (states[row][cell]) {
-          html += "alive";
-        } else {
-      html += "dead";
+    if (states) {
+      for (var row = 0; row < states.length; row++) {
+        html += "<tr>";
+        for (var cell = 0; cell < states[row].length; cell++) {
+          html += "<td class='";
+          if (states[row][cell]) {
+            html += "alive";
+          } else {
+            html += "dead";
+          }
+          html += "''></td>";
         }
-        html += "''></td>";
+        html += "</tr>";
       }
-      html += "</tr>";
-  }
-  html += "</table>";
+      this.model.save(null, {success: this.serverSuccess, error: this.serverError});
+    }
+    html += "</table>";
+    for (var i = 0; i < this.infoMessages.models.length; i++) {
+      html += this.infoMessages.models[i].attributes.messageText + "<br/>";
+    }
     this.$el.html(html);
     sleep(100);
-    this.model.save();
   }
 });
 
+// Start
 var boardModel = new BoardModel();
 var appView = new AppView({model: boardModel});
